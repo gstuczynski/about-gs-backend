8dc6623efd28534f4f6bdbd3222aaca0568ea055
Grzegorz Stuczyński <grzegorz.stuczynski@evidenceprime.com>
2018-12-26 13:22:20 +0100

    fixed asset path

--- routes/assets.js
+++ routes/assets.js
@@ -8,7 +8,7 @@ router.get("/asset", (req, res) => {
     res.status(400).send({ message: "file_required" });
     return;
   }
-  res.sendFile(`${__dirname}/assets/${file}`, err => {
+  res.sendFile(`../assets/${file}`, err => {
     if (err) {
       res.status(500).send({ message: "couldn't get data" });
       console.error("couldn't get data: ", err);
